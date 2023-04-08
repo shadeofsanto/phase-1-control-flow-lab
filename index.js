0607eae82be531f716699ac617a0494d2c1d50e8
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-04-08 13:16:26 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if(someNumber <= 400)
+    return "This one is on me!"
+  else if (someNumber >= 400 && someNumber <= 2000)
+    return "That will be twenty bucks."
+  else if (someNumber >= 2000 && someNumber <= 2500)
+    return "I will gladly take your thirty bucks."
+  else
+    return "No can do."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(terNumber){
+  return terNumber === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+function switchOnCharmFromTip(tipGen){
+  switch (tipGen) {
+    case "generous" :
+      return "Thank you so much.";
+        break;
+    case "not as generous" :
+      return "Thank you.";
+        break;
+    default:
+      return "Bye."
+  }
 }
